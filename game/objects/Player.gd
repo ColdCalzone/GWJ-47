38d844a004f2564a5cdc3173afe88420622d46e8
ColdCalzone <coldhotpocket1@gmail.com>
2022-07-09 20:56:59 -0400

    Jumping feels good now

--- game/objects/Player.gd
+++ game/objects/Player.gd
@@ -1,14 +1,25 @@
+# TODO fix animation when jumping
+# TODO Add left facing jumps
+
 extends RigidBody2D
 
 const MAX_SPEED : float = 200.0
-const ACCEL : float = 20.0
-const JUMP = -150
+const ACCEL : float = 1200.0
+const JUMP_MAX_SPEED : float = -300.0
+const JUMP_IMPUSE : float = -350.0
+const JUMP_DEACCEL : float = 5.0
+const FRICTION : float = -720.0
 
+# Animation
 onready var body_anim : AnimatedSprite = $Body
 onready var tread_anim : AnimatedSprite = $Treads
-onready var coyote : Timer = $Coyote
+
+# Timers for that nice jumping
 onready var jump : Timer = $JumpMin
 onready var jump_max : Timer = $JumpMax
+
+var coyote : float = 0.25
+const COYOTE_MAX : float = 0.25
 
 # Collision stuff
 onready var up : RayCast2D = $Up
@@ -20,27 +31,24 @@ export var flipped : bool = false
 var flipped_coefficient : float = 1
 var tread_speed : float = 1
 
+# States the player enters
+# Not doing a proper Finite State Machine, but that may be something I need to change.
 enum State {
 	Grounded
 	Jumping
 	Airborn
 }
 
-# so hack very why
-enum AnimationState {
-	NotJumping
-	Jumping
-}
-
 var state = State.Grounded
-var anim_state = AnimationState.NotJumping
 
 func _ready():
 	flipped_coefficient = int(flipped) * 2 - 1
 
+# Animation stuff is handled here
 func _process(delta):
+	# right = 1 left = -1
 	var direction = (Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")) * flipped_coefficient
-	if(direction != 0) and anim_state == AnimationState.NotJumping:
+	if(direction != 0) and state == State.Grounded:
 		if direction > 0:
 			body_anim.animation = "move_right" 
 		elif direction < 0:
@@ -49,37 +57,56 @@ func _process(delta):
 	else:
 		body_anim.playing = false
 	
-	if Input.is_action_just_pressed("jump") and anim_state != AnimationState.Jumping:
+	if state == State.Jumping and body_anim.animation != "jump":
 		body_anim.animation = "jump"
-		anim_state = AnimationState.Jumping
 		body_anim.playing = true
 	
-	if anim_state == AnimationState.Jumping:
-		if body_anim.frame == 3:
+	if state == State.Jumping || state == State.Airborn:
+		if body_anim.frame == 1:
 			body_anim.playing = false
+		if Input.is_action_just_released("jump"):
+			body_anim.playing = true
+			body_anim.animation = "move_right"
 	
 	if abs(linear_velocity.x) > 0:
+		# Dynamic tread speed :)
 		tread_anim.speed_scale = max(abs(linear_velocity.x) / 50.0, 0.8)
 		tread_anim.playing = true
 	else:
 		tread_anim.playing = false
 
 func _physics_process(delta):
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and state != State.Airborn:
+		state = State.Jumping
 		jump.start()
-		
+		jump_max.start()
+	# if we're jumping, and you've either stopped pressing jump or the maximum jump length is done, and the minimum jump length is done...
+	elif state == State.Jumping and (!Input.is_action_pressed("jump") || jump_max.is_stopped()) and jump.is_stopped():
+		state = State.Airborn
+		jump_max.stop()
 	if(down.is_colliding() and state != State.Grounded and jump.is_stopped()):
-		anim_state = AnimationState.NotJumping
 		state = State.Grounded
+	elif !down.is_colliding() and state == State.Grounded and coyote > 0:
+		coyote -= delta
+	elif down.is_colliding() and state == State.Airborn:
+		state = State.Grounded
+	if down.is_colliding() and coyote < COYOTE_MAX:
+		coyote = COYOTE_MAX
+	if coyote <= 0:
+		state = State.Airborn
 
 func _integrate_forces(_state):
+	var step = _state.get_step()
 	var direction = (Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")) * flipped_coefficient
-	linear_velocity.x += ACCEL * direction
-	if Input.is_action_pressed("jump") and (jump_max.time_left > 0 || jump_max.is_stopped()) and state != State.Airborn:
-		linear_velocity.y = JUMP
-		state = State.Jumping
-		if jump_max.is_stopped():
-			jump_max.start()
-	if state == State.Jumping and !Input.is_action_just_pressed("jump"):
-		state = State.Airborn
+	linear_velocity.x += (ACCEL * direction) * step
+	var vx = abs(linear_velocity.x)
+	vx += FRICTION * step
+	if vx < 0: vx = 0
+	linear_velocity.x = vx * sign(linear_velocity.x)
+	if state == State.Jumping:
+		if linear_velocity.y >= 0: linear_velocity.y = JUMP_IMPUSE #clamp(linear_velocity.y + JUMP_ACCEL, JUMP_MAX_SPEED, 0)
+		linear_velocity.y += JUMP_DEACCEL * step
+	if state == State.Airborn:
+		linear_velocity.y = max(linear_velocity.y, JUMP_IMPUSE / 2)
+		linear_velocity.y + JUMP_DEACCEL * step * 1.5
 	linear_velocity.x = clamp(linear_velocity.x, -MAX_SPEED, MAX_SPEED)
