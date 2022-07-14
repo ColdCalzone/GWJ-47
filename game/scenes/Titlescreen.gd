72ba9925d6058c534df55cb46f2fa5368e2f4261
ColdCalzone <coldhotpocket1@gmail.com>
2022-07-14 10:11:22 -0400

    Logo on titlescreen

--- game/scenes/Titlescreen.gd
+++ game/scenes/Titlescreen.gd
@@ -1,5 +1,8 @@
 extends Control
 
+onready var logo = $CenterContainer2
+onready var tween = $Tween
+
 func _ready():
 	Bg.visible = Settings.backgrounds
 
@@ -17,3 +20,12 @@ func _on_Quit_pressed():
 
 func _on_Credits_pressed():
 	TransitionManager.transition_to(Scenes.Scenes.Credits)
+
+
+
+func _on_TextureRect_gui_input(event):
+	if tween.is_active(): return
+	if event.is_action_pressed("click"):
+		tween.interpolate_property(logo, "rect_scale:y", logo.rect_scale.y, -logo.rect_scale.y, 0.75, Tween.TRANS_CIRC)
+		tween.start()
+
